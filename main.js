1b16a5ccb5893cbb9e58cd78e84062426b38376f
Przemek Juzwiszin <przemekjuzwiszin@gmail.com>
2020-08-07 09:09:45 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log ('siema');
+const name = 'Maciek Korsan';
+const age = 31;
+let maciek = null;
+console.log (name);
+console.log (age);
+
+console.log (`Nazywam się ${name} i mam ${age} lat`);
+
+console.log('witaj przybyszu')
