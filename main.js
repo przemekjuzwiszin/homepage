e15924ffcf096cf8f106e97efdcb6d300db84393
Przemek Juzwiszin <przemekjuzwiszin@gmail.com>
2020-09-20 19:59:49 +0200

    use document.querySelector in JavaScript

--- main.js
+++ main.js
@@ -8,3 +8,7 @@ console.log (age);
 console.log (`Nazywam się ${name} i mam ${age} lat`);
 
 console.log('witaj przybyszu')
+
+const header__title = document.querySelector('.header__title');
+
+console.log(header__title);
